069bb5cf69a5ef14f96940d9d5e69a0c16878ea5
tsune-s <your.email@example.com>
2025-05-06 10:23:05 +0900

    テスト環境

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,7 +13,7 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
-  title: "オフィスツネ",
+  title: "どもどもテスト環境です",
   description: "オフィスツネのポートフォリオサイトです。",
   icons: {
     icon: "/favicon.png",
